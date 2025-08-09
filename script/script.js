610edb31aa761c27c4b3ba05575688c93c1c0ddb
TigranPoghos <tigran2002poghosyan@gmail.com>
2025-08-09 17:33:07 +0300

    haybay

---
+++ script/script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    gsap.registerPlugin(ScrollTrigger);
+    const mm = gsap.matchMedia();
+    const tl = gsap.timeline();
+
+    tl.to('.titleGsap', {
+        backgroundSize: "100% 100%",
+        duration: 2,
+        ease: "power2.inOut",
+        delay: 0.5,
+    })
+
+    tl.to('.titleGsap', {
+        left: 'auto',
+        bottom: 'auto',
+        duration: 1,
+    })
+
+    tl.to('.titleGsap', {
+        position: 'relative',
+        margin: '0 0 35px 0',
+        color: 'black',
+        duration: 0.5
+    })
+
+    tl.to('.main__fon', {
+        width: 0,
+        height: 0,
+        duration: 0.5
+    }, '<')
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+})
+
+
+
+
